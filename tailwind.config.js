aee156e4bd9734994b37924e36903b7122b625a2
Ivaylo Ivanov <ivailo1224@gmail.com>
2024-08-24 14:56:28 +0300

    Init Push

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      "colors": {
+      "color-rose-800": "rgba(122, 40, 78, 1)",
+      "color-rose-50": "rgba(255, 247, 251, 1)",
+      "color-stone-900": "rgba(49, 46, 44, 1)",
+      "color-stone-600": "rgba(95, 86, 77, 1)",
+      "color-stone-150": "rgba(227, 221, 215, 1)",
+      "color-stone-100": "rgba(243, 229, 215, 1)",
+      "color-brown-800": "rgba(133, 70, 50, 1)",
+      "color-white": "rgba(255, 255, 255, 1)"
+    },
+    "fontFamily": {
+      "young-serif": "Young Serif",
+      "outfit": "Outfit"
+    }
+    },
+  },
+  plugins: [],
+}
+
